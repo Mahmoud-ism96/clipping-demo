5c071ed0ca799ad26f567351df3652e8b483bc2a
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2023-01-15 03:55:07 +0200

    Added the fourth rectangle fun

--- app/src/main/java/com/example/clippingdemo/ClippedView.kt
+++ app/src/main/java/com/example/clippingdemo/ClippedView.kt
@@ -89,7 +89,7 @@ class ClippedView @JvmOverloads constructor(
 
         canvas.save()
         canvas.translate(columnOne,rowOne)
-        
+
         drawClippedRectangle(canvas)
         canvas.restore()
     }
@@ -143,6 +143,32 @@ class ClippedView @JvmOverloads constructor(
         canvas.restore()
     }
     private fun drawIntersectionClippingExample(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(columnTwo,rowTwo)
+        
+        canvas.clipRect(
+            clipRectLeft,clipRectTop,
+            clipRectRight - smallRectOffset,
+            clipRectBottom - smallRectOffset
+        )
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRight,clipRectBottom,
+                Region.Op.INTERSECT
+            )
+        } else {
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRight,clipRectBottom
+            )
+        }
+
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
     private fun drawCombinedClippingExample(canvas: Canvas) {
     }
